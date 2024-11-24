9b4a05c7005af2c4c7d568492847037dde7f450d
omaranBazna <omran4d@gmail.com>
2024-11-23 22:51:30 -0500

    upload course plan

--- src/components/StudentCourses.js
+++ src/components/StudentCourses.js
@@ -1,12 +1,39 @@
-
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 export default function StudentCourses({studentCourses}){
 
     if(!studentCourses){
         return <></>
     }
+    let {needed,token} = studentCourses
+    needed = needed.flat()
     return <div>
-        {JSONG.stringify(studentCourses)}
+        <h1>Student courses</h1>
+<div style={{display:"flex",alignItems:"start",justifyContent:"center",gap:10}}>
+      <table>
+        <tr>
+            <th>Needed</th>
+        </tr>
+        {needed.map(element=>{
+            return <tr>
+                <td>{element}</td>
+            </tr>
+        })}
+      </table>
+    
+      <table>
+        <tr>
+            <th>Token course</th>
+            <th>Grade</th>
+        </tr>
+        {token.map(element=>{
+            return <tr>
+                <td>{element.course}</td>
+                <td>{element.grade}</td>
+            </tr>
+        })}
+      </table>
+    </div>
     </div>
 }
