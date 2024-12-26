7e8ecfb693cb130b49211a863678dfdac0f1d7fc
omaranBazna <omran4d@gmail.com>
2024-12-25 22:25:07 -0500

    changes

--- src/components/BasicTreeView.js
+++ src/components/BasicTreeView.js
@@ -162,8 +162,7 @@ export default function BasicTreeView(){
             if (regex.test(course)) {
                 
                 let result = await checkSingleCourse(course)
-                console.log(course)
-                console.log(result);
+
                 if(result){
                     newCourses.push(course);
                 }
@@ -472,7 +471,13 @@ initialDate={"2024-11-17"}
       headerToolbar={{ left: "prev,next today", center: "title", right: "timeGridWeek,timeGridDay" }}
       dayHeaderContent={(day) =>day.text.substring(0,3)}
       height={"100%"}
-      
+      eventClick={(info)=>{
+        let code = info.event._def.title.split("-")[0].trim()
+        setEvents((old)=>{
+        return old.filter(item=>item.code !=code)
+        
+        })
+      }}
     />
   </div>
 }
